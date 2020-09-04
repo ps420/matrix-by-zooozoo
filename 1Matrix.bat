4446a3ca705b21bbdd3a2e929911b5ea1f94bdf9
ps420 <41146549+ps420@users.noreply.github.com>
2020-09-04 19:06:43 +0200

    Add files via upload

---
+++ 1Matrix.bat
@@ -0,0 +1,20 @@
+@echo off
+title Matrix
+color 0a
+mode 1000
+goto Greeting
+
+
+:greeting
+cls
+echo Hello Zayan
+pause
+cls
+goto Matrix
+
+
+:Matrix
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%tatatattattatatatVVVVVVVVVVV%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto Matrix
+PING 1.1.1.1 -n 1 -w 0.9 >NUL
+goto Matrix
